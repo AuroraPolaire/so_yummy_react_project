86a3e7704533d67701f97dfcfa1584b502f583e3
AuroraPolaire <sea.forest.mountains@gmail.com>
2023-04-11 00:38:51 +0300

    feat: added redux store + persistor, installed dependencies

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// import { PersistGate } from 'redux-persist/integration/react';
+// import { persistor } from './redux/store';
+// import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
-import './index.css';
+// import { store } from './redux/store';
 import { GlobalStyles } from 'components/theme/GlobalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* <Provider store={store}> */}
+    {/* <PersistGate loading={null} persistor={persistor}></PersistGate> */}
     <BrowserRouter basename="/so_yummy_react_project">
       <GlobalStyles />
       <App />
     </BrowserRouter>
+    {/* </Provider> */}
   </React.StrictMode>
 );
