b8e5bc99271881cd9726464299f09fdaae5c0f6a
Anhelina Shabrina <sh.angelina97@gmail.com>
2023-04-11 15:00:41 +0300

    adds logo

--- src/components/Logo/Logo.styled.js
+++ src/components/Logo/Logo.styled.js
@@ -1,10 +1,13 @@
-import React from 'react';
-// import PropTypes from 'prop-types'
+import styled from 'styled-components';
+import { NavLink as Link } from 'react-router-dom';
 
-const Logo = props => {
-  return <div>Logo</div>;
-};
-
-// Logo.propTypes = {}
-
-export default Logo;
+export const LogoImg = styled.img`
+  width: 40px;
+  height: 40px;
+  @media (min-width: 768px) {
+    width: 44px;
+    height: 44px;
+    margin-right: 168px;
+  }
+`;
+export const LogoLink = styled(Link)``;
