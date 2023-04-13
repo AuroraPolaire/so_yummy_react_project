f33c36224af4b7a8e1a6724c2197278e30fd838c
AuroraPolaire <sea.forest.mountains@gmail.com>
2023-04-13 15:43:57 +0300

    feat: link to recipe id

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { RecipeCardBox } from './RecipeCard.styled';
 
 const RecipeCard = ({ recipe }) => {
+  function truncateTitle(str) {
+    if (str.length > 28) {
+      return str.slice(0, 28) + '...';
+    } else {
+      return str;
+    }
+  }
   console.log(recipe);
   return recipe.map(item => {
     return (
-      <RecipeCardBox key={item._id}>
-        <img src={item.preview} alt={item.title} />
-        <p className="title">{item.title}</p>
-      </RecipeCardBox>
+      <NavLink to={`/recipe/${item._id}`}>
+        <RecipeCardBox key={item._id}>
+          <img src={item.preview} alt={item.title} />
+          <p className="title">{truncateTitle(item.title)}</p>
+        </RecipeCardBox>
+      </NavLink>
     );
   });
 };
