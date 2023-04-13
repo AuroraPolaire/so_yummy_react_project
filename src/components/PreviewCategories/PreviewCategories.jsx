cd5bd5f3866cf82884fd5eef0d0a611c464aa464
AuroraPolaire <sea.forest.mountains@gmail.com>
2023-04-13 13:35:51 +0300

    feat: rendering recipe cards according to device media

--- src/components/PreviewCategories/PreviewCategories.jsx
+++ src/components/PreviewCategories/PreviewCategories.jsx
@@ -9,24 +9,31 @@ const PreviewCategories = () => {
   const randomRecipes = useSelector(selectRecipesMainPage);
   const list = Object.entries(randomRecipes);
 
-  // let mobileMedia = window.matchMedia('(max-width: 376px)');
-  // let tabletMedia = window.matchMedia(
-  //   '(min-width: 377px) and (max-width: 768px)'
-  // );
-  // let desktopMedia = window.matchMedia('(min-width: 769px)');
-  // console.log(mobileMedia);
-  // console.log(tabletMedia);
+  let mobileMedia = window.matchMedia('(max-width: 376px)');
+  let tabletMedia = window.matchMedia(
+    '(min-width: 377px) and (max-width: 769px)'
+  );
+  let desktopMedia = window.matchMedia('(min-width: 770px)');
 
-  // console.log(desktopMedia);
-
-  // console.log((document.querySelector('.mq-value').innerText = mql.matches));
+  const filteredRecipesList = recipe => {
+    if (!recipe) return;
+    if (mobileMedia.matches) {
+      const mobileList = recipe.slice(0, 1);
+      return mobileList;
+    } else if (tabletMedia.matches) {
+      const tabletList = recipe.slice(0, 2);
+      return tabletList;
+    } else if (desktopMedia.matches) {
+      return recipe;
+    }
+  };
 
   return list.flatMap(item => {
     return (
       <div key={item[0]}>
         <PageTitle type="mainPage">{item[0]}</PageTitle>
         <List>
-          <RecipeCard recipe={item[1]} />
+          <RecipeCard recipe={filteredRecipesList(item[1])} />
           {/* {item[1].map(item => {
             return (
               <div className="card" key={item._id}>
--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -1,27 +1,26 @@
 import React from 'react';
 
 const RecipeCard = ({ recipe }) => {
-  let mobileMedia = window.matchMedia('(max-width: 376px)');
-  let tabletMedia = window.matchMedia(
-    '(min-width: 377px) and (max-width: 769px)'
-  );
-  let desktopMedia = window.matchMedia('(min-width: 770px)');
+  // let mobileMedia = window.matchMedia('(max-width: 376px)');
+  // let tabletMedia = window.matchMedia(
+  //   '(min-width: 377px) and (max-width: 769px)'
+  // );
+  // let desktopMedia = window.matchMedia('(min-width: 770px)');
 
-  const filteredRecipesList = recipe => {
-    if (!recipe) return;
-    if (mobileMedia.matches) {
-      const mobileList = recipe.slice(0, 1);
-      return mobileList;
-    } else if (tabletMedia.matches) {
-      const tabletList = recipe.slice(0, 2);
-      return tabletList;
-    } else if (desktopMedia.matches) {
-      return recipe;
-    }
-  };
-  console.log(filteredRecipesList(recipe));
+  // const filteredRecipesList = recipe => {
+  //   if (!recipe) return;
+  //   if (mobileMedia.matches) {
+  //     const mobileList = recipe.slice(0, 1);
+  //     return mobileList;
+  //   } else if (tabletMedia.matches) {
+  //     const tabletList = recipe.slice(0, 2);
+  //     return tabletList;
+  //   } else if (desktopMedia.matches) {
+  //     return recipe;
+  //   }
+  // };
 
-  return filteredRecipesList(recipe).map(item => {
+  return recipe.map(item => {
     return (
       <div className="card" key={item._id}>
         <img src={item.preview} alt={item.title} />
