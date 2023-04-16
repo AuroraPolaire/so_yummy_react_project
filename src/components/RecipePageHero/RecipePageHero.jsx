0f75f8a0e068f5119701ce33c0f55314fd038239
AuroraPolaire <sea.forest.mountains@gmail.com>
2023-04-16 22:54:49 +0300

    fix: changed button logic, added styles

--- src/components/RecipePageHero/RecipePageHero.jsx
+++ src/components/RecipePageHero/RecipePageHero.jsx
@@ -1,5 +1,5 @@
 import Time from 'components/Time/Time';
-import React from 'react';
+import React, { useState } from 'react';
 import { RecipeBackground, RecipeHeroBox } from './RecipePageHero.styled';
 import SVG from '../../images/icons/sprite.svg';
 import { Section, Wrapper } from 'components/theme/GlobalContainer';
@@ -13,7 +13,8 @@ export default function RecipePageHero({
   time,
   id,
 }) {
-  // const [favouriteRecipe, setFavourite] = useState(false);
+  console.log(favorite);
+  const [favouriteRecipe, setFavourite] = useState(favorite);
   const dispatch = useDispatch();
   return (
     <RecipeBackground>
@@ -22,23 +23,21 @@ export default function RecipePageHero({
           <RecipeHeroBox>
             <h1>{title}</h1>
             {description && <p>{description}</p>}
+            <button
+              className={favouriteRecipe ? 'favourite' : 'not-favourite'}
+              onClick={() =>
+                dispatch(
+                  toggleFavouriteRecipes(id),
+                  setFavourite(!favouriteRecipe)
+                )
+              }
+            >
+              {favouriteRecipe
+                ? 'Remove from favorite recipes'
+                : 'Add to favorite recipes'}
+            </button>
 
-            {/* {favorite ? (
-              <button onClick={() => dispatch(toggleFavouriteRecipes(id))}>
-                Remove from favorite recipes
-              </button>
-            ) : (
-              <button
-                onClick={
-                  (() => setFavourite(true),
-                  dispatch(toggleFavouriteRecipes(id)))
-                }
-              >
-                Add to favorite recipes
-              </button>
-            )} */}
-
-            {!favorite && (
+            {/* {!favorite && (
               <button onClick={dispatch(toggleFavouriteRecipes(id))}>
                 Add to favorite recipes
               </button>
@@ -47,7 +46,7 @@ export default function RecipePageHero({
               <button onClick={() => dispatch(toggleFavouriteRecipes(id))}>
                 Remove from favorite recipes
               </button>
-            )}
+            )} */}
             {time.length > 0 && (
               <div>
                 <svg>
