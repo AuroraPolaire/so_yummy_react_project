176dc57acf368c0410dcf8e845d5b6711a9798e7
Anhelina Shabrina <sh.angelina97@gmail.com>
2023-04-17 19:35:12 +0300

    Fixes user components styles

--- src/components/Header/UserLogo/UserLogo.jsx
+++ src/components/Header/UserLogo/UserLogo.jsx
@@ -1,40 +1,36 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-// import { authReducer } from '../../../redux/auth/authSlice';
+import { useMediaQuery } from '@mui/material';
+
 import {
   selectUser,
   selectIsLoggedIn,
 } from '../../../redux/auth/authSelectors';
 
+import { ReactComponent as DefaultAvatar } from '../../../images/icons/default-avatar.svg';
 import {
-  UserAvatar,
+  EditWrapper,
   UserAvatarWrapper,
   UserLogoButton,
   UserName,
   UserNameWrapper,
 } from './UserLogo.styled';
 
-// import UserLogoModal from '../UserLogoModal/UserLogoModal';
 import UserInfoModal from '../UserInfoModal/UserInfoModal';
 import LogoutConfirmation from '../LogoutConfirmation/LogoutConfirmation';
 
-import {
-  Dialog,
-  Popper,
-  ClickAwayListener,
-  Paper,
-  Button,
-  // Typography,
-} from '@mui/material';
+import { Dialog, Popper, ClickAwayListener, Paper } from '@mui/material';
 
-// import Avatar from '@mui/material/Avatar';
-// import IconButton from '@mui/material/IconButton';
+import Avatar from '@mui/material/Avatar';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import EditIcon from '@mui/icons-material/Edit';
+import Tooltip from '@mui/material/Tooltip';
 
 import { LogoutBtn } from '../UserLogo/UserLogo.styled';
 
 const UserLogo = () => {
+  const isTablet = useMediaQuery('(min-width: 768px)');
+
   const isLoggedIn = useSelector(selectIsLoggedIn);
   const { name, avatarURL } = useSelector(selectUser);
 
@@ -72,55 +68,53 @@ const UserLogo = () => {
     <>
       {isLoggedIn && (
         <>
-          <UserLogoButton
-            id="user-logo-button"
-            type="button"
-            onClick={onUserLogoClick}
-          >
-            <UserAvatarWrapper>
-              <UserAvatar
-                style={{ backgroundImage: `url(${avatarURL})` }}
-              ></UserAvatar>
-            </UserAvatarWrapper>
+          <Tooltip title="You may update your information here">
+            <UserLogoButton
+              id="user-logo-button"
+              type="button"
+              onClick={onUserLogoClick}
+            >
+              <UserAvatarWrapper>
+                <Avatar
+                  alt="avatar"
+                  src={avatarURL || DefaultAvatar}
+                  sx={{
+                    height: { xs: 34, sm: 40, md: 44 },
+                    width: { xs: 34, sm: 40, md: 44 },
+                  }}
+                />
+                {/* <UserAvatar
+                  style={{
+                    backgroundImage: `url(${avatarURL})`,
+                  }}
+                ></UserAvatar> */}
+              </UserAvatarWrapper>
 
-            <UserNameWrapper>
-              <UserName>{name || 'UserName'}</UserName>
-            </UserNameWrapper>
-          </UserLogoButton>
+              <UserNameWrapper>
+                <UserName>{name || 'User name'}</UserName>
+              </UserNameWrapper>
+            </UserLogoButton>
+          </Tooltip>
 
           <Popper open={popperOpen} anchorEl={anchorEl} placement="bottom">
             <ClickAwayListener onClickAway={onUserLogoClose}>
               <Paper
                 style={{
-                  width: '177px',
-                  height: '134px',
-                  padding: '10px',
+                  width: isTablet ? '177px' : '161px',
+                  height: isTablet ? '134px' : '130px',
+                  padding: '18px',
                   display: 'flex',
                   flexDirection: 'column',
                   justifyContent: 'space-between',
                   border: '1px solid #8BAA36',
                   boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-                  mt: 1.5,
+                  marginTop: isTablet ? 15 : 14,
                 }}
               >
-                <Button
-                  onClick={onUserInfoOpen}
-                  sx={{
-                    fontSize: 14,
-                    color: '#22252A',
-                    textDecoration: 'none',
-                    fontFamily: 'Poppins, sans-serif',
-                    p: 0,
-                    display: {
-                      xs: 'flex',
-                      md: 'flex',
-                      justifyContent: 'space-between',
-                    },
-                  }}
-                >
+                <EditWrapper onClick={onUserInfoOpen}>
                   Edit profile
-                  <EditIcon sx={{ ml: '50px' }} />
-                </Button>
+                  <EditIcon sx={{ fontSize: 18 }} />
+                </EditWrapper>
 
                 <LogoutBtn onClick={onLogoutOpen}>
                   Log out <ArrowForwardIcon sx={{ fontSize: 18 }} />
