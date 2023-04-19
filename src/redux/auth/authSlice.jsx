66194bd97ebf2f40d98cfb3bf7b0483528063b62
AuroraPolaire <sea.forest.mountains@gmail.com>
2023-04-19 16:57:18 +0300

    fix: deleted consolelogs

--- src/redux/auth/authSlice.jsx
+++ src/redux/auth/authSlice.jsx
@@ -27,20 +27,11 @@ const authSlice = createSlice({
   extraReducers: builder => {
     builder
       .addCase(register.fulfilled, (state, action) => {
-        console.log(action);
-        // state.user = action.payload.user;
-        state.user.name = action.payload.user.name;
-        state.user.email = action.payload.user.email;
-
-        // state.token = action.payload.accessToken;
-        // state.refreshToken = action.payload.refreshToken;
+        state.user = action.payload.user;
         state.isLoggedIn = true;
       })
       .addCase(signIn.fulfilled, (state, action) => {
-        console.log(action);
-        // state.user = action.payload.user;
-        state.user.name = action.payload.user.name;
-        state.user.email = action.payload.user.email;
+        state.user = action.payload.user;
         state.token = action.payload.accessToken;
         state.refreshToken = action.payload.refreshToken;
         state.isLoggedIn = true;
